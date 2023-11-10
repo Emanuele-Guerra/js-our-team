08938a6e8f6ce6bd99ac64190d255ff80151ac8d
Emanuele-Guerra <emaguerra1993@hotmail.it>
2023-11-10 16:28:32 +0100

    end all log

---
+++ js/main.js
@@ -0,0 +1,38 @@
+const person = [
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        pic: "wayne-barnett-founder-ceo.jpg"
+    },
+    {
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        pic: "angela-caroll-chief-editor.jpg"
+    },
+    {
+        name: "Walter Gordon",
+        role: "Office Scapegoat",
+        pic: "walter-gordon-office-manager.jpg"
+    },
+    {
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        pic: "angela-lopez-social-media-manager.jpg"
+    },
+    {
+        name: "Scott Estrada",
+        role: "Developer",
+        pic: "scott-estrada-developer.jpg"
+    },
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        pic: "barbara-ramos-graphic-designer.jpg"
+    }
+];
+for (let i = 0; i < person.length; i++) {
+    console.log(person[i].name)
+    console.log(person[i].role)
+    console.log(person[i].pic)
+}
+
